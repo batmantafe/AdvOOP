4acf123d93a699080b8f7be67f4bf68e0657ed24
Duane Reymond <duane.reymond@gmail.com>
2018-04-30 18:06:23 +1000

    Added more to 3-Commands.

---
+++ Unity/Assets/#Examples/3-Commands/Scripts/Common/Player/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commands
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        public float lookSpeed = 5f;
+        public float maxVelocity = 5f;
+
+        [Header("References")]
+        public Transform attachedCamera;
+
+        // Private References
+        private Rigidbody rigid;
+
+        private float inputH, inputV;
+        private float mouseX, mouseY;
+
+        #region Unity Functions
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            LimitVelocity();
+
+            PerformMove();
+            PerformLook();
+        }
+        #endregion
+        
+        public void PerformMove()
+        {
+            Vector3 inputDir = new Vector3(inputH, 0, inputV);
+
+            float yDegrees = attachedCamera.eulerAngles.y;
+            Quaternion localRotation = Quaternion.Euler(0, yDegrees, 0);
+            inputDir = localRotation * inputDir;
+
+            rigid.velocity = inputDir * moveSpeed;
+        }
+
+        void PerformLook()
+        {
+            // Rotate Player on the "Horizontal" axis
+            Vector3 euler = transform.eulerAngles;
+            euler.y += mouseX * lookSpeed * Time.deltaTime;
+            transform.eulerAngles = euler;
+
+            // Rotate Camera on the "Vertical" axis
+            Vector3 camEuler = attachedCamera.localEulerAngles;
+            camEuler.x -= mouseY * lookSpeed * Time.deltaTime;
+            attachedCamera.localEulerAngles = camEuler;
+        }
+
+        #region Custom Functions
+        void LimitVelocity()
+        {
+            Vector3 vel = rigid.velocity;
+
+            if (vel.magnitude > maxVelocity)
+            {
+                vel = vel.normalized * maxVelocity;
+            }
+
+            rigid.velocity = vel;
+        }
+
+        public void Look (float horizontal, float vertical)
+        {
+            mouseX = horizontal;
+            mouseY = vertical;
+
+            // Perform any delegate actions here
+        }
+
+        public void Move (float horizontal, float vertical)
+        {
+            inputH = horizontal;
+            inputV = vertical;
+
+            // Perform any delegate actions here
+        }
+        #endregion
+    }
+}
---
+++ Unity/Assets/#Examples/3-Commands/Scripts/Multiplayer/NetworkUserInput.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+namespace Commands
+{
+    [RequireComponent(typeof(PlayerController))]
+
+    public class NetworkUserInput : NetworkBehaviour
+    {
+        public Camera cam;
+        public AudioListener aud;
+
+        [SyncVar] public float inputH, inputV;
+        [SyncVar] public float mouseX, mouseY;
+
+        private PlayerController player;
+
+        #region Unity Functions
+        void Start()
+        {
+            player = GetComponent<PlayerController>();
+
+            cam.enabled = isLocalPlayer;
+            aud.enabled = isLocalPlayer;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isLocalPlayer)
+            {
+                GetInput();
+
+                player.Move(inputH, inputV);
+                player.Look(mouseX, mouseY);
+
+                // Send input across network
+                Cmd_Move(inputH, inputV);
+                Cmd_Look(mouseX, mouseY);
+            }
+        }
+        #endregion
+        
+        #region Custom Function
+        void GetInput()
+        {
+            inputH = Input.GetAxis("Horizontal");
+            inputV = Input.GetAxis("Vertical");
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
+        }
+        #endregion
+
+        #region Commands
+        [Command] void Cmd_Move(float inputH, float inputV)
+        {
+            Rpc_Move(inputH, inputV);
+        }
+
+        [Command] void Cmd_Look(float mouseX, float mouseY)
+        {
+            Rpc_Look(mouseX, mouseY);
+        }
+        #endregion
+
+        // Functions that get run by the server and run on each client
+        #region Remote Procedure Calls (RPCs)
+        [ClientRpc] void Rpc_Move(float inputH, float inputV)
+        {
+            player.Move(inputH, inputV);
+        }
+
+        [ClientRpc] void Rpc_Look(float mouseX, float mouseY)
+        {
+            player.Look(mouseX, mouseY);
+        }
+        #endregion
+    }
+}
